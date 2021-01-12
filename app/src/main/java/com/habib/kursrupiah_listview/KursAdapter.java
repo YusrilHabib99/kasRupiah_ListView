8b3098fed18cf2d4674c14455d821bf1efcb5e92
Yusril <yusrilhabib99@gmail.com>
2021-01-12 22:59:07 +0800

    ini pengetesan

---
+++ app/src/main/java/com/habib/kursrupiah_listview/KursAdapter.java
@@ -0,0 +1,61 @@
+package com.habib.kursrupiah_listview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.List;
+
+public class KursAdapter extends ArrayAdapter<NilaiKurs> {
+
+    private static DecimalFormat formatUang;
+    private Context context;
+
+    public KursAdapter (@NonNull Context context, @NonNull List<NilaiKurs> object) {
+        super(context, R.layout.row_kurs, object);
+        this.context = context;
+        siapkanFormatUang();
+    }
+
+    private static class ViewHolder {
+        TextView txMataUang;
+        TextView txNilaiKurs;
+    }
+
+    @NonNull
+    @Override
+    public  View getView (int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        NilaiKurs kurs = getItem(position);
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.row_kurs, parent, false);
+            viewHolder.txMataUang = convertView.findViewById(R.id.rowTextMataUang);
+            viewHolder.txNilaiKurs = convertView.findViewById(R.id.rowTextNilaiKurs);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.txMataUang.setText("1 "+kurs.getMataUang());
+        String nilaiKurs = formatUang.format(kurs.getNilaiKurs());
+        viewHolder.txNilaiKurs.setText(nilaiKurs+" IDR");
+        return convertView;
+    }
+
+    private static  void siapkanFormatUang() {
+        formatUang = new DecimalFormat("#,###.00");
+        DecimalFormatSymbols s = new DecimalFormatSymbols();
+        s.setGroupingSeparator('.');
+        s.setDecimalSeparator(',');
+        formatUang.setDecimalFormatSymbols(s);
+    }
+}
---
+++ app/src/main/java/com/habib/kursrupiah_listview/MainActivity.java
@@ -0,0 +1,98 @@
+package com.habib.kursrupiah_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tglKurs;
+    FloatingActionButton btnRefresh;
+    ProgressBar loadingIndicator;
+    JSONObject kursTerbaru;
+    ListView listNilaiKurs;
+    ArrayAdapter<NilaiKurs> adapterKurs;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inisialisasiView();
+        ambilDataKurs();
+    }
+
+    private void inisialisasiView() {
+        tglKurs = findViewById(R.id.tglKurs);
+        loadingIndicator = findViewById(R.id.loadingIndicator);
+        btnRefresh = findViewById(R.id.btnRefresh);
+        btnRefresh.setOnClickListener(view -> ambilDataKurs());
+        listNilaiKurs = findViewById(R.id.listNilaiKurs);
+        adapterKurs = new KursAdapter(this, new ArrayList<>());
+        listNilaiKurs.setAdapter(adapterKurs);
+    }
+
+    private void ambilDataKurs() {
+        loadingIndicator.setVisibility(View.VISIBLE);
+        String baseURL = "https://api.exchangeratesapi.io/latest?base=IDR";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, baseURL, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("MAIN", response.toString());
+                        kursTerbaru = response;
+
+                        refreshView();
+                        loadingIndicator.setVisibility(View.GONE);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        loadingIndicator.setVisibility(View.GONE);
+                        Toast.makeText(getApplicationContext(),"Gagal mengambil data", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
+    }
+
+    private void refreshView() {
+        adapterKurs.clear();
+        try {
+            tglKurs.setText(kursTerbaru.getString("date"));
+            List<NilaiKurs> nilaiKursList = new ArrayList<>();
+            JSONObject rates = kursTerbaru.getJSONObject("rates");
+            for (Iterator<String> it = rates.keys(); it.hasNext();) {
+                String mataUang = it.next();
+                double nilaiTukar = 1/rates.getDouble(mataUang);
+                NilaiKurs nilaiKurs = new NilaiKurs(mataUang, nilaiTukar);
+                nilaiKursList.add(nilaiKurs);
+            }
+            adapterKurs.addAll(nilaiKursList);
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
